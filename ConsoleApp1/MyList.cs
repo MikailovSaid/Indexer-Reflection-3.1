5108a9799624d91f7791f92dab6485b2a36fdd1d
Said G. Mikayilov <saidgm@code.local>
2021-12-20 10:03:02 +0400

    Homework

---
+++ ConsoleApp1/MyList.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Extention
+{
+    public class MyList <T>
+    {
+        public T[] list;
+        public int count;
+        public string List;
+        public MyList()
+        {
+            list = new T[10];
+        }
+        private void ArrayResize()
+        {
+            Array.Resize(ref list, list.Length + 10);
+        }
+        public void Add(T obj)
+        {
+            if ((count != 0) && (count % 10 == 0))
+            {
+                ArrayResize();
+            }
+            list[count] = obj;
+            count++;
+            Console.WriteLine(obj);
+        }
+        public void Clear()
+        {
+            list = new T[10];
+            count = 0;
+        }
+        public void AddRange(T list)
+        {
+            Add(list);
+        }
+        public bool Contains(T obj)
+        {
+            foreach (var item in list)
+            {
+                if (Comparer<T>.Default.Compare(item, obj) == 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+
+        }
+        public T Find(Predicate<T> func)
+        {
+            foreach (var item in list)
+            {
+                if (func(item))
+                {
+                    return item;
+                }
+            }
+            return default;
+
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Extention
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<object> list = new MyList<object>();
+            list.Add("Said");
+            list.Add("Salam");
+            list.Add(1);
+            list.Add(2);
+
+            list.AddRange(3);
+
+            //list.Clear();
+
+            //list.Find();
+        }
+    }
+}
